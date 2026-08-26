ca3fa586e7b43273195f0113db16c209ed454475
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-26 06:19:22 -0500

    Capture CEF media context metadata

--- include/goreecloud/browser/cef_client.hpp
+++ include/goreecloud/browser/cef_client.hpp
@@ -2,12 +2,15 @@
 
 #include <functional>
 #include <mutex>
+#include <optional>
 #include <string>
 
 #include "goreecloud/browser/engine.hpp"
+#include "goreecloud/browser/media_target_detector.hpp"
 
 #if GOREECLOUD_ENABLE_CEF
 #include "include/cef_client.h"
+#include "include/cef_context_menu_handler.h"
 #include "include/wrapper/cef_helpers.h"
 #endif
 
@@ -18,19 +21,24 @@ namespace goreecloud::browser {
 class GoreeCloudCefClient final : public CefClient,
                                   public CefDisplayHandler,
                                   public CefLifeSpanHandler,
-                                  public CefLoadHandler {
+                                  public CefLoadHandler,
+                                  public CefContextMenuHandler {
  public:
   using NavigationCallback = std::function<void(const NavigationState&)>;
   using ClosedCallback = std::function<void()>;
+  using MediaContextCallback = std::function<void(const RawMediaHitTest&)>;
 
   GoreeCloudCefClient(NavigationCallback navigation_callback,
-                      ClosedCallback closed_callback)
+                      ClosedCallback closed_callback,
+                      MediaContextCallback media_context_callback = {})
       : navigation_callback_(std::move(navigation_callback)),
-        closed_callback_(std::move(closed_callback)) {}
+        closed_callback_(std::move(closed_callback)),
+        media_context_callback_(std::move(media_context_callback)) {}
 
   CefRefPtr<CefDisplayHandler> GetDisplayHandler() override { return this; }
   CefRefPtr<CefLifeSpanHandler> GetLifeSpanHandler() override { return this; }
   CefRefPtr<CefLoadHandler> GetLoadHandler() override { return this; }
+  CefRefPtr<CefContextMenuHandler> GetContextMenuHandler() override { return this; }
 
   void OnAfterCreated(CefRefPtr<CefBrowser> browser) override {
     CEF_REQUIRE_UI_THREAD();
@@ -84,7 +92,59 @@ class GoreeCloudCefClient final : public CefClient,
     publish();
   }
 
+  void OnBeforeContextMenu(CefRefPtr<CefBrowser>,
+                           CefRefPtr<CefFrame> frame,
+                           CefRefPtr<CefContextMenuParams> params,
+                           CefRefPtr<CefMenuModel>) override {
+    CEF_REQUIRE_UI_THREAD();
+    if (!params) return;
+
+    RawMediaHitTest hit;
+    hit.page_url = frame ? frame->GetURL().ToString() : state_.url;
+    hit.media_url = params->GetSourceUrl().ToString();
+    hit.link_url = params->GetLinkUrl().ToString();
+    hit.mime_type = params->GetSourceMimeType().ToString();
+    hit.alt_text = params->GetTitleText().ToString();
+
+    const auto media_type = params->GetMediaType();
+    switch (media_type) {
+      case CM_MEDIATYPE_IMAGE:
+        hit.kind = MediaKind::image;
+        hit.copyable = true;
+        hit.downloadable = !hit.media_url.empty();
+        hit.region_selectable = true;
+        hit.ocr_supported = true;
+        break;
+      case CM_MEDIATYPE_VIDEO:
+        hit.kind = MediaKind::video;
+        hit.copyable = !hit.media_url.empty();
+        hit.downloadable = !hit.media_url.empty();
+        hit.frame_capture_supported = true;
+        hit.region_selectable = true;
+        hit.ocr_supported = true;
+        break;
+      default:
+        hit.kind = MediaKind::unknown;
+        break;
+    }
+
+    const auto flags = params->GetTypeFlags();
+    hit.linked = (flags & CM_TYPEFLAG_LINK) != 0 && !hit.link_url.empty();
+    hit.secure_resource = hit.media_url.rfind("https://", 0) == 0;
+
+    {
+      std::scoped_lock lock(media_mutex_);
+      last_media_context_ = hit;
+    }
+    if (media_context_callback_) media_context_callback_(hit);
+  }
+
   [[nodiscard]] CefRefPtr<CefBrowser> browser() const { return browser_; }
+
+  [[nodiscard]] std::optional<RawMediaHitTest> last_media_context() const {
+    std::scoped_lock lock(media_mutex_);
+    return last_media_context_;
+  }
 
  private:
   void publish() {
@@ -95,6 +155,9 @@ class GoreeCloudCefClient final : public CefClient,
   NavigationState state_;
   NavigationCallback navigation_callback_;
   ClosedCallback closed_callback_;
+  MediaContextCallback media_context_callback_;
+  mutable std::mutex media_mutex_;
+  std::optional<RawMediaHitTest> last_media_context_;
 
   IMPLEMENT_REFCOUNTING(GoreeCloudCefClient);
   DISALLOW_COPY_AND_ASSIGN(GoreeCloudCefClient);
