d657b533d0cb6d85d4ebde914c8d7d887103703d
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-26 00:41:06 -0500

    feat: add browser service registry

---
+++ include/goreecloud/browser/service_registry.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <memory>
+
+#include "goreecloud/browser/services.hpp"
+
+namespace goreecloud::browser {
+
+struct BrowserServices {
+  std::shared_ptr<SearchService> search;
+  std::shared_ptr<BookmarkService> bookmarks;
+  std::shared_ptr<SyncService> sync;
+  std::shared_ptr<VaultService> vault;
+  std::shared_ptr<DnsService> dns;
+  std::shared_ptr<NetworkService> network;
+  std::shared_ptr<PrivacyShieldService> privacy_shield;
+  std::shared_ptr<WardveilService> wardveil;
+};
+
+class ServiceRegistry {
+ public:
+  explicit ServiceRegistry(BrowserServices services)
+      : services_(std::move(services)) {}
+
+  [[nodiscard]] const BrowserServices& services() const noexcept {
+    return services_;
+  }
+
+ private:
+  BrowserServices services_;
+};
+
+}  // namespace goreecloud::browser
