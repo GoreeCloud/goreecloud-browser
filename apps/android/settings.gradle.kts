1958351c648836e0bf5884f312be37937534ec55
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-26 01:39:49 -0500

    Add Android application scaffold

---
+++ apps/android/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GoreeCloudBrowser"
+include(":app")
