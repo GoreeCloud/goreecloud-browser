739281305489b8a900e8ed530222354d649c89bc
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-26 14:49:09 -0500

    Add CEF Media Hover preview message contract

--- include/goreecloud/browser/cef_media_probe.hpp
+++ include/goreecloud/browser/cef_media_probe.hpp
@@ -10,6 +10,8 @@ namespace goreecloud::browser {
 
 inline constexpr char kMediaProbeRequestMessage[] = "goreecloud.media_probe.request";
 inline constexpr char kMediaProbeResponseMessage[] = "goreecloud.media_probe.response";
+inline constexpr char kMediaPreviewRequestMessage[] = "goreecloud.media_preview.request";
+inline constexpr char kMediaPreviewResponseMessage[] = "goreecloud.media_preview.response";
 
 struct MediaProbePoint {
   int x{0};
@@ -29,9 +31,11 @@ class CefMediaProbeBridge {
 inline constexpr bool kMediaProbeMayTriggerNetworkProcessing = false;
 inline constexpr bool kMediaProbeMayTriggerAi = false;
 inline constexpr bool kMediaProbeMayTriggerOcr = false;
+inline constexpr bool kMediaPreviewMayBypassCanvasSecurity = false;
 
 static_assert(!kMediaProbeMayTriggerNetworkProcessing);
 static_assert(!kMediaProbeMayTriggerAi);
 static_assert(!kMediaProbeMayTriggerOcr);
+static_assert(!kMediaPreviewMayBypassCanvasSecurity);
 
 }  // namespace goreecloud::browser
