0b2a77258323e7709ab3598de2f2fda677bf330c
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-26 02:34:12 -0500

    Add Everkeep to GoreeCloud settings

--- include/goreecloud/browser/settings_surface.hpp
+++ include/goreecloud/browser/settings_surface.hpp
@@ -19,6 +19,7 @@ enum class SettingsSection {
   permissions,
   containers,
   sync_and_identity,
+  everkeep,
   memory_management,
   private_browsing,
   accessibility,
@@ -40,6 +41,7 @@ inline constexpr std::array kSettingsSections{
     SettingsSection::permissions,
     SettingsSection::containers,
     SettingsSection::sync_and_identity,
+    SettingsSection::everkeep,
     SettingsSection::memory_management,
     SettingsSection::private_browsing,
     SettingsSection::accessibility,
@@ -51,10 +53,12 @@ inline constexpr bool kSettingsSearchEnabled = true;
 inline constexpr bool kSettingsUseProgressiveDisclosure = true;
 inline constexpr bool kInheritedSettingsTaxonomyAllowed = false;
 inline constexpr bool kSettingsUseLatestApprovedGlaze = true;
+inline constexpr bool kEverkeepFirstClassSettingsArea = true;
 
 static_assert(kSettingsSearchEnabled);
 static_assert(kSettingsUseProgressiveDisclosure);
 static_assert(!kInheritedSettingsTaxonomyAllowed);
 static_assert(kSettingsUseLatestApprovedGlaze);
+static_assert(kEverkeepFirstClassSettingsArea);
 
 }  // namespace goreecloud::browser
